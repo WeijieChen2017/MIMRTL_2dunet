f7527e51318d249eabf358b6d08a7fb5baf03a80
WeijieChen2017 <weijiechen1994@gmail.com>
2020-05-02 22:42:41 -0500

    slice 5 support

--- S2_Generate_Dataset.py
+++ S2_Generate_Dataset.py
@@ -39,7 +39,7 @@ def slice5(dataA, dataB, name_dataset, n_slice=1, name_tag="", resize_f=1):
     for idx_z in range(z):
         for idx_c in range(n_slice):
             print(int(index[idx_z, idx_c]))
-            print(dataA[:, :, index[idx_z, idx_c]].shape)
+            print(dataA[:, :, int(index[idx_z, idx_c])].shape)
             img[idx_c, :, :w] = zoom(dataA[:, :, int(index[idx_z, idx_c])], zoom=resize_f)
             img[idx_c, :, w:] = zoom(dataB[:, :, int(index[idx_z, idx_c])], zoom=resize_f)
         name2save = path2save+name_tag+"_"+str(idx_z)+".npy"
