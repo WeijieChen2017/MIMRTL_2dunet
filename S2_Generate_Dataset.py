d7cfab3f75c99a43f9e16c7eeb752105a5720be4
WeijieChen2017 <weijiechen1994@gmail.com>
2020-05-26 03:41:05 -0500

    v1

--- S2_Generate_Dataset.py
+++ S2_Generate_Dataset.py
@@ -82,31 +82,31 @@ for path_ori in list_ori:
     slice5_A(dataA=data_ori, name_dataset=name_dataset, n_slice=n_slice, name_tag=filename_ori, resize_f = 1)
     print("------------------------------------------------------------------------")
 
-# list_ori = glob.glob("./data/"+name_dataset+"/pure/*.nii")
-# list_ori.sort()
-# for path_ori in list_ori:
-#     print("TrainA:")
-#     filename_ori = os.path.basename(path_ori)[:]
-#     filename_ori = filename_ori[:filename_ori.find(".")]
-#     print(filename_ori)
-#     data_ori = maxmin_norm(nib.load(path_ori).get_fdata())
+list_ori = glob.glob("./data/"+name_dataset+"/pure/*.nii")
+list_ori.sort()
+for path_ori in list_ori:
+    print("TrainA:")
+    filename_ori = os.path.basename(path_ori)[:]
+    filename_ori = filename_ori[:filename_ori.find(".")]
+    print(filename_ori)
+    data_ori = maxmin_norm(nib.load(path_ori).get_fdata())
     
-#     list_sim = glob.glob("./data/"+name_dataset+"/blur/*"+filename_ori+"*.nii")
-#     list_sim.sort()
+    list_sim = glob.glob("./data/"+name_dataset+"/blur/*"+filename_ori+"*.nii")
+    list_sim.sort()
     
-#     for path_sim in list_sim:
-#         print("Pairs")
-#         filename_sim = os.path.basename(path_sim)[:]
-#         filename_sim = filename_sim[:filename_sim.find(".")]
-#         print("A:", filename_ori)
-#         print("B:", filename_sim)
+    for path_sim in list_sim:
+        print("Pairs")
+        filename_sim = os.path.basename(path_sim)[:]
+        filename_sim = filename_sim[:filename_sim.find(".")]
+        print("A:", filename_ori)
+        print("B:", filename_sim)
                 
-#         data_sim = maxmin_norm(nib.load(path_sim).get_fdata())
-#         slice5_AB(dataA=data_ori, dataB=data_sim,
-#                   name_dataset=name_dataset, n_slice=n_slice,
-#                   name_tag=filename_sim, resize_f=1)
+        data_sim = maxmin_norm(nib.load(path_sim).get_fdata())
+        slice5_AB(dataA=data_ori, dataB=data_sim,
+                  name_dataset=name_dataset, n_slice=n_slice,
+                  name_tag=filename_sim, resize_f=1)
         
-#     print("------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------")
         
         
         
--- S5_Assembler.py
+++ S5_Assembler.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 from scipy.ndimage import zoom
 
-name_dataset = "sk8R"
+name_dataset = "phantom"
 resize_f = 1
-n_slice = 21
+n_slice = 7
 # test_folder = "testB"
 
 # list_ori = glob.glob("./data/"+name_dataset+"/test/*.nii")
